640fe246aa2697c559b21bca4de6dfab1fa4180c
makotot <makoto.ttn@gmail.com>
2022-02-18 01:26:02 +0900

    use msw

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const request = async () => {
+    const response = await (await fetch('/hello')).json()
+    console.log(response)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -14,13 +14,16 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+
+      <button type="button" onClick={request}>
+        hello
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
